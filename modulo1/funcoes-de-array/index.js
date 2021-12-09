015ea291bdc1557d27855f588dfc97477e7d799e
Leo Rigotto <leorigotto@me.com>
2021-12-09 14:22:50 -0300

    melhoria de código em funcoes-de-array

--- modulo1/funcoes-de-array/index.js
+++ modulo1/funcoes-de-array/index.js
@@ -62,18 +62,18 @@
 
 // EXERCÍCIO 2
 
-    // const products = [
-    // { nome: "Alface Lavada", categoria: "Hortifruti", preco: 2.5 },
-    // { nome: "Guaraná 2l", categoria: "Bebidas", preco: 7.8 },
-    // { nome: "Veja Multiuso", categoria: "Limpeza", preco: 12.6 },
-    // { nome: "Dúzia de Banana", categoria: "Hortifruti", preco: 5.7 },
-    // { nome: "Leite", categoria: "Bebidas", preco: 2.99 },
-    // { nome: "Cândida", categoria: "Limpeza", preco: 3.30 },
-    // { nome: "Detergente Ypê", categoria: "Limpeza", preco: 2.2 },
-    // { nome: "Vinho Tinto", categoria: "Bebidas", preco: 55 },
-    // { nome: "Berinjela kg", categoria: "Hortifruti", preco: 8.99 },
-    // { nome: "Sabão em Pó Ypê", categoria: "Limpeza", preco: 10.80 }
-    // ]
+    const products = [
+    { nome: "Alface Lavada", categoria: "Hortifruti", preco: 2.5 },
+    { nome: "Guaraná 2l", categoria: "Bebidas", preco: 7.8 },
+    { nome: "Veja Multiuso", categoria: "Limpeza", preco: 12.6 },
+    { nome: "Dúzia de Banana", categoria: "Hortifruti", preco: 5.7 },
+    { nome: "Leite", categoria: "Bebidas", preco: 2.99 },
+    { nome: "Cândida", categoria: "Limpeza", preco: 3.30 },
+    { nome: "Detergente Ypê", categoria: "Limpeza", preco: 2.2 },
+    { nome: "Vinho Tinto", categoria: "Bebidas", preco: 55 },
+    { nome: "Berinjela kg", categoria: "Hortifruti", preco: 8.99 },
+    { nome: "Sabão em Pó Ypê", categoria: "Limpeza", preco: 10.80 }
+    ]
 
 // a.
 
@@ -83,9 +83,13 @@
 
 // b.
 
-    // const fiveOffProducts = products.map((item) => {
-    //     return item.nome + " " + Number((item.preco - (item.preco * 0.05)).toFixed(2))
-    // })
+    const fiveOffProducts = products.map((item) => {
+        return {
+            nome: item.nome,
+            preco: Number((item.preco - (item.preco * 0.05)).toFixed(2))
+        }
+    })
+    console.log(fiveOffProducts)
 
 // c.
 
@@ -126,4 +130,4 @@
         return item.tipo
     })
     const typesUnique = [...new Set(types)]
-    console.log(typesUnique)
+    // console.log(typesUnique)
