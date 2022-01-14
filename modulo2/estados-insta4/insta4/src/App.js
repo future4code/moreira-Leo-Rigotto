85759de187f940e5d1b715767712d6abddf1c581
Leo Rigotto <leorigotto@me.com>
2022-01-13 22:02:05 -0300

    posts tratados para dentro do state

--- modulo2/estados-insta4/insta4/src/App.js
+++ modulo2/estados-insta4/insta4/src/App.js
@@ -9,25 +9,46 @@ const MainContainer = styled.div`
   align-items: center;
 `
 
+const NewPostContainer = styled.div`
+  display: flex;
+  flex: column;
+`
+
+const NewPostInput = styled.input`
+  
+`
+
 class App extends React.Component {
+
+  state = {
+    posts: [
+      {nomeUsuario: 'fefa',
+      fotoUsuario: "https://picsum.photos/50/50",
+      fotoPost: 'https://picsum.photos/200/150'},
+
+      {nomeUsuario: 'larry',
+      fotoUsuario: "https://picsum.photos/50/51",
+      fotoPost: 'https://picsum.photos/200/151'},
+
+      {nomeUsuario: 'letisga',
+      fotoUsuario: "https://picsum.photos/50/52",
+      fotoPost: 'https://picsum.photos/200/152'}
+    ]
+  }
+
   render() {
+
+    const post = this.state.posts.map((user) => {
+      return <Post
+      nomeUsuario={user.nomeUsuario}
+      fotoUsuario={user.fotoUsuario}
+      fotoPost={user.fotoPost}
+      />
+    })
+
     return (
       <MainContainer>
-        <Post
-          nomeUsuario={'paulinha'}
-          fotoUsuario={'https://picsum.photos/50/50'}
-          fotoPost={'https://picsum.photos/200/150'}
-        />
-        <Post
-          nomeUsuario={'larry'}
-          fotoUsuario={'https://picsum.photos/50/51'}
-          fotoPost={'https://picsum.photos/200/152'}
-        />
-        <Post
-          nomeUsuario={'letisga'}
-          fotoUsuario={'https://picsum.photos/50/52'}
-          fotoPost={'https://picsum.photos/200/153'}
-        />
+        {post}
       </MainContainer>
     );
   }
