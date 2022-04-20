adae60630ff4d1c2d23a2120e70df0956d21292e
Leo Rigotto <leorigotto@me.com>
2022-04-19 22:34:46 -0300

    ex 3

--- modulo4/async-await-promises/src/ex3.ts
+++ modulo4/async-await-promises/src/ex3.ts
@@ -1,1 +1,23 @@
-// 
+// a) não
+// b) para garantir que não estamos recebendo dados faltantes?
+
+import axios from "axios"
+
+// c)
+
+type user = {
+  id: string
+  name: string
+  email: string
+}
+
+const getSubscribers = async (): Promise<user[]> => {
+  const response = await axios.get(`${BASE_URL}/subscribers`)
+  return response.data.map((res: any) => {
+    return {
+      id: res.id,
+      name: res.name,
+      email: res.email,
+    }
+  })
+}
